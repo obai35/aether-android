1c3469e356ed5036886557d07407989a54e7bc37
obai35 <obai35@users.noreply.github.com>
2026-08-21 07:50:04 +0300

    Fix MainActivity: navArgument import from androidx.navigation

--- app/src/main/java/com/aether/companion/ui/MainActivity.kt
+++ app/src/main/java/com/aether/companion/ui/MainActivity.kt
@@ -21,8 +21,8 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.aether.companion.R
 import com.aether.companion.data.api.NetworkModule
 import com.aether.companion.data.model.AutomationEvent
